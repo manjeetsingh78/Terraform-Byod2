0e80f9483918186b4561724a635686b5defb453d
Manjeet Singh <149746052+manjeetsingh78@users.noreply.github.com>
2025-12-11 15:35:02 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -5,4 +5,4 @@ amazon-linux-extras install nginx1 -y
 systemctl start nginx
 systemctl enable nginx
 
-echo "<h1>Project Genesis SUCCESS! Deployed by YOUR NAME</h1>" > /usr/share/nginx/html/index.html
+echo "<h1>Project Genesis SUCCESS! Deployed by Manjeet Singh</h1>" > /usr/share/nginx/html/index.html
